f8a87841e22be63cc9bdd498ce1a84ecc34e6796
Pritesh Vardekar <priteshv.264@gmail.com>
2023-11-26 23:26:51 +0530

    First version 1.0 done

---
+++ script.js
@@ -0,0 +1,28 @@
+const inputext = document.getElementById("inputext");
+const len = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^()_+~|}{[]></-=";
+const allchar = upperCase + lowerCase + number + symbol;
+function copy() {
+    inputext.select();
+    document.execCommand("copy");
+}
+
+function generate() {
+    let password = "";
+
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (password.length < len) {
+        console.log(password.length);
+        password += allchar[Math.floor(Math.random() * allchar.length)];
+    }
+    inputext.value = password;
+    // console.log(password);
+}

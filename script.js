1107221db506e754bfa59bf476c786655210c0ca
Pritesh Vardekar <priteshv.264@gmail.com>
2023-12-03 17:04:52 +0530

    version 1.5 done/

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const inputext = document.getElementById("inputext");
-const len = 12;
+const range = document.getElementById("length");
+const numb = document.getElementById("number");
+const upper = document.getElementById("upper");
+const lower = document.getElementById("lower");
+const sym = document.getElementById("sym");
+const numPass = document.getElementById("num");
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "@#$%^()_+~|}{[]></-=";
 const allchar = upperCase + lowerCase + number + symbol;
+
+function rangeFunc(){
+    numb.value = range.value;
+    return numb.value;
+  }
+
+  function numFunc(){
+      range.value = numb.value;
+  }
+
 function copy() {
     inputext.select();
     // document.execCommand("copy");  ---> this execCommand() function is deprecated. Hence used below function
@@ -20,8 +35,8 @@ function generate() {
     password += number[Math.floor(Math.random() * number.length)];
     password += symbol[Math.floor(Math.random() * symbol.length)];
 
-    while (password.length < len) {
-        console.log(password.length);
+    while (password.length < rangeFunc()) {
+        // console.log(password.length);
         password += allchar[Math.floor(Math.random() * allchar.length)];
     }
     inputext.value = password;
